c8feaa6f6802c0c4ac8aaa54bf1f030d1de78694
Maike <maikerdralcantara@gmail.com>
2025-03-26 22:28:35 -0300

    add multiply

--- functions.lua
+++ functions.lua
@@ -1,24 +1,25 @@
 function add(a, b)
-  return a + b
-end 
+	return a + b
+end
 
 function subtract(a, b)
-  return a - b
+	return a - b
 end
 
 function multiply(a, b)
-  return a * b
+	return a * b
 end
 
 function divide(a, b)
-  if b~=0 then
-    return a/b
-  else
-    return "Error: Division by zero"
-  end
+	if b ~= 0 then
+		return a / b
+	else
+		return "Error: Division by zero"
+	end
 end
 
-print("addition: " .. add(4,2))
+print("addition: " .. add(4, 2))
 print("subtraction: " .. subtract(3, 1))
-print("divide by 0: " ..divide(2,0))
-print("divide: " ..divide(10, 5))
+print("multiply: " .. subtract(3, 1))
+print("divide by 0: " .. divide(2, 0))
+print("divide: " .. divide(10, 5))
